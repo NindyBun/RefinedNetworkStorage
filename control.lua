67061f428a841c2eccbb97f22c14a2b62c624a81
NindyBun <andyco5665@gmail.com>
2023-06-05 06:35:32 -0700

    qwe

--- control.lua
+++ control.lua
@@ -1,1 +1,10 @@
 Constants = require("utils.constants")
+script.on_init(function()
+    local freeplay = remote.interfaces["freeplay"]
+    if freeplay then  -- Disable freeplay popup-message
+        if freeplay["set_skip_intro"] then remote.call("freeplay", "set_skip_intro", true) end
+        if freeplay["set_disable_crashsite"] then remote.call("freeplay", "set_disable_crashsite", true) end
+    end
+
+    global.players = {}
+end)
