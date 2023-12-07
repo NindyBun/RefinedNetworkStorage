d71cf91007ea94d9593197fd37f6106d9f90d1f0
NindyBun <andyco5665@gmail.com>
2023-12-07 04:39:20 -0800

    woops

--- prototypes/NetworkCableIOFluid.lua
+++ prototypes/NetworkCableIOFluid.lua
@@ -38,7 +38,7 @@ ioR.result_count = 1
 data:extend{ioR}
 ]]
 
-local ioE = {}
+--[[local ioE = {}
 ioE.type = "storage-tank"
 ioE.name = Constants.NetworkCables.fluidIO.name
 ioE.icon = Constants.NetworkCables.fluidIO.itemIcon
@@ -118,4 +118,70 @@ ioE.pictures.picture.south.layers[2].x = 512*2
 ioE.pictures.picture.west = table.deepcopy(ioE.pictures.picture.north)
 ioE.pictures.picture.west.layers[1].x = 512*3
 ioE.pictures.picture.west.layers[2].x = 512*3
+data:extend{ioE}]]
+
+local ioE = {}
+ioE.type = "assembling-machine"
+ioE.name = Constants.NetworkCables.fluidIO.name
+ioE.icon = Constants.NetworkCables.fluidIO.itemIcon
+ioE.icon_size = 512
+ioE.flags = {"placeable-neutral", "player-creation"}
+ioE.collision_box = {{-0.40, -0.40}, {0.40, 0.40}}
+ioE.selection_box = {{-0.5, -0.5}, {0.5, 0.5}}
+ioE.fast_replaceable_group = Constants.Settings.RNS_FR_Cable
+ioE.max_health = 350
+ioE.dying_explosion = "medium-explosion"
+ioE.corpse = "small-remnants"
+ioE.open_sound = { filename = "__base__/sound/wooden-chest-open.ogg" }
+ioE.close_sound = { filename = "__base__/sound/wooden-chest-close.ogg" }
+ioE.vehicle_impact_sound =  { filename = "__base__/sound/car-wood-impact.ogg", volume = 1.0 }
+ioE.minable = {mining_time = 0.2, result = Constants.NetworkCables.fluidIO.name}
+ioE.animation =
+    {
+        north = {
+			layers = {
+				{
+					filename = Constants.NetworkCables.fluidIO.entityE,
+					priority = "extra-high",
+                    size = 512,
+					scale = 1/8,
+					x=0
+				},
+				{
+					filename = Constants.NetworkCables.fluidIO.entityS,
+					priority = "high",
+                    size = 512,
+					draw_as_shadow = true,
+					scale = 1/8,
+					x=0
+				}
+			}
+		}
+    }
+ioE.animation.east = table.deepcopy(ioE.animation.north)
+ioE.animation.east.layers[1].x = 512
+ioE.animation.east.layers[2].x = 512
+ioE.animation.south = table.deepcopy(ioE.animation.north)
+ioE.animation.south.layers[1].x = 512*2
+ioE.animation.south.layers[2].x = 512*2
+ioE.animation.west = table.deepcopy(ioE.animation.north)
+ioE.animation.west.layers[1].x = 512*3
+ioE.animation.west.layers[2].x = 512*3
+ioE.crafting_categories = {"RNS-Nothing"}
+ioE.crafting_speed = 1
+ioE.energy_source =
+{
+    type = "void",
+}
+ioE.energy_usage = "1J"
+ioE.fluid_boxes = {
+    {
+        base_area = 1,
+		hide_connection_info = true,
+        pipe_connections = {
+            {type = "output", position = {0, -1}}
+        },
+        production_type = "output"
+    }
+}
 data:extend{ioE}
