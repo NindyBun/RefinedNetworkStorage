d789f46bd7c09af1349f23f03d98c9c9ed707c26
NindyBun <andyco5665@gmail.com>
2024-03-24 14:23:53 -0700

    e

--- scripts/Events.lua
+++ scripts/Events.lua
@@ -191,17 +191,23 @@ function Event.finished_research(event)
 	local name, _ = string.gsub(event.research.name, "%-", "_")
 	local level = event.research.level
 	if string.match(name, "RNS_item_transfer_bonus") ~= nil then
+		local old = global.IIOMultiplier
 		global.IIOMultiplier = string.match(name, "infinite") == nil and Constants.Settings.Multipliers.IIO[level] or (Constants.Settings.Multipliers.IIO[8] + 2*level)
 		for _, obj in pairs(global["ItemIOTable"]) do
-			obj.stackSize = math.min(obj.stackSize, global.IIOMultiplier)
+			if obj.stackSize == old then
+				obj.stackSize = global.IIOMultiplier
+			end
 		end
 		--printResearchBonus("item")
 		return
 	end
 	if string.match(name, "RNS_fluid_transfer_bonus") ~= nil then
+		local old = global.FIOMultiplier
 		global.FIOMultiplier = string.match(name, "infinite") == nil and Constants.Settings.Multipliers.FIO[level] or (Constants.Settings.Multipliers.FIO[8] + 2*level)
 		for _, obj in pairs(global["FluidIOTable"]) do
-			obj.fluidSize = math.min(obj.fluidSize, global.FIOMultiplier)
+			if obj.fluidSize == old then
+				obj.fluidSize = global.FIOMultiplier
+			end
 		end
 		--printResearchBonus("fluid")
 		return
